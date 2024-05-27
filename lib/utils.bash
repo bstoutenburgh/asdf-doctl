18e7e07a5d7e29026eaa2624810586836468a12f
Ben Stoutenburgh <maristgeek@gmail.com>
2024-05-27 18:40:37 -0400

    adapt bin/list-all

---
+++ lib/utils.bash
@@ -0,0 +1,73 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+# TODO: Ensure this is the correct GitHub homepage where releases can be downloaded for <YOUR TOOL>.
+GH_REPO="https://github.com/digitalocean/doctl"
+TOOL_NAME="doctl"
+TOOL_TEST="doctl version"
+
+fail() {
+	echo -e "asdf-$TOOL_NAME: $*"
+	exit 1
+}
+
+curl_opts=(-fsSL)
+
+# NOTE: You might want to remove this if <YOUR TOOL> is not hosted on GitHub releases.
+if [ -n "${GITHUB_API_TOKEN:-}" ]; then
+	curl_opts=("${curl_opts[@]}" -H "Authorization: token $GITHUB_API_TOKEN")
+fi
+
+sort_versions() {
+	sed 'h; s/[+-]/./g; s/.p\([[:digit:]]\)/.z\1/; s/$/.z/; G; s/\n/ /' |
+		LC_ALL=C sort -t. -k 1,1 -k 2,2n -k 3,3n -k 4,4n -k 5,5n | awk '{print $2}'
+}
+
+list_github_tags() {
+	git ls-remote --tags --refs "$GH_REPO" |
+		grep -o 'refs/tags/.*' | cut -d/ -f3- |
+		grep -v '^[^v]' | # remove tags 1, prototype-1 and anything else that does not start with v
+		sed 's/^v//'
+}
+
+list_all_versions() {
+	list_github_tags
+}
+
+download_release() {
+	local version filename url
+	version="$1"
+	filename="$2"
+
+	# TODO: Adapt the release URL convention for <YOUR TOOL>
+	url="$GH_REPO/archive/v${version}.tar.gz"
+
+	echo "* Downloading $TOOL_NAME release $version..."
+	curl "${curl_opts[@]}" -o "$filename" -C - "$url" || fail "Could not download $url"
+}
+
+install_version() {
+	local install_type="$1"
+	local version="$2"
+	local install_path="${3%/bin}/bin"
+
+	if [ "$install_type" != "version" ]; then
+		fail "asdf-$TOOL_NAME supports release installs only"
+	fi
+
+	(
+		mkdir -p "$install_path"
+		cp -r "$ASDF_DOWNLOAD_PATH"/* "$install_path"
+
+		# TODO: Assert <YOUR TOOL> executable exists.
+		local tool_cmd
+		tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
+		test -x "$install_path/$tool_cmd" || fail "Expected $install_path/$tool_cmd to be executable."
+
+		echo "$TOOL_NAME $version installation was successful!"
+	) || (
+		rm -rf "$install_path"
+		fail "An error occurred while installing $TOOL_NAME $version."
+	)
+}
